0c072b06142ae9c67f069c5a0c1cf3ece520b90a
h8wait <14755912+h8wait@users.noreply.github.com>
2022-12-14 18:53:52 +0100

    Creating a framework for building a kit for the app

---
+++ Petosaur/PetosaurKit/PetosaurKit.h
@@ -0,0 +1,18 @@
+//
+//  PetosaurKit.h
+//  PetosaurKit
+//
+//  Created by h8wait on 14.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PetosaurKit.
+FOUNDATION_EXPORT double PetosaurKitVersionNumber;
+
+//! Project version string for PetosaurKit.
+FOUNDATION_EXPORT const unsigned char PetosaurKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PetosaurKit/PublicHeader.h>
+
+
